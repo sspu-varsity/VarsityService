b2ff9a0a8d1cf539197050197c9a775f8fa47c4c
Willemhia <1491615493@qq.com>
2022-07-09 12:30:05 +0800

    Initial commit

---
+++ src/main/java/com/sspu/varsityservice/VarsityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sspu.varsityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarsityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarsityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sspu/varsityservice/VarsityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspu.varsityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarsityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
