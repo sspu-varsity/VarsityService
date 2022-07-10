60d22e8b3e94e1d757aea32e17f6685c3950825a
Willemhia <1491615493@qq.com>
2022-07-10 17:55:12 +0800

    修改文件

--- src/main/java/com/sspu/varsityservice/configuration/WebConfiguration.java
+++ src/main/java/com/sspu/varsityservice/configuration/WebConfiguration.java
@@ -2,9 +2,7 @@ package com.sspu.varsityservice.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.concurrent.Executors;
 
@@ -23,5 +21,23 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowedOrigins("*");
+    }
+
+    //设置访问图片的虚拟路径映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        //文件磁盘图片url 映射 ，一下位windows下的
+        //配置server虚拟路径，handler为前台访问的目录，locations为files相对应的本地路径
+        registry.addResourceHandler("/images/**").addResourceLocations("file:D:\\images\\");
+
+
+        /************************下边为linux系统下的 ******/
+        // registry.addResourceHandler("/image/**").addResourceLocations("file:/home/userImages/");
+        /*  registry.addResourceHandler("/carouselImages/**").addResourceLocations("file:/home/carouselImages/");
+        registry.addResourceHandler("/courseImages/**").addResourceLocations("file:/home/courseImages/");
+        registry.addResourceHandler("/teacherImages/**").addResourceLocations("file:/home/teacherImages/");
+        registry.addResourceHandler("/systemImages/**").addResourceLocations("file:/home/systemImages/");
+        registry.addResourceHandler("/articleImages/**").addResourceLocations("file:/home/articleImages/");*/
     }
 }
